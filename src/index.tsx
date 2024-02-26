4d6d583269c95d9c5536afb24d1205f497bf30ef
valtermartins1301 <valtermartins.1301@gmail.com>
2024-02-26 17:12:52 -0300

    add SWR to fetch content

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,36 @@
 import React from 'react';
+import './global.css';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
+import Characters from './Pages/Characters/Characters';
+import { SWRConfig, Cache } from 'swr';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Characters />,
+  },
+]);
+
+function sessionStorageProvider() {
+  const map = new Map(JSON.parse(sessionStorage.getItem('app-cache') || '[]'));
+
+  window.addEventListener('beforeunload', () => {
+    const appCache = JSON.stringify(Array.from(map.entries()));
+    sessionStorage.setItem('app-cache', appCache);
+  });
+
+  return map as Cache;
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <SWRConfig value={{ provider: sessionStorageProvider }}>
+      <RouterProvider router={router} />
+    </SWRConfig>
+  </React.StrictMode>,
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
